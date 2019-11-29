867a506dcdf63230da8f9125a523240fc51edffe
lprabha <lprabha47@gmail.com>
2019-11-29 14:07:28 +0545

    Add app file

---
+++ app/src/main/java/com/example/two_wheeler_schedule_management_system/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.two_wheeler_schedule_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
